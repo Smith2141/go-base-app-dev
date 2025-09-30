e77542a3392a757003962c9a26c5a77c5fd5d491
maxim <acer.laptop>
2025-09-30 22:40:06 +0300

    Set variables 2

--- cmd/main.go
+++ cmd/main.go
@@ -3,21 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// Объявляем целочисленную переменную
-	var number int
-	// Объявляем две строковые переменные
-	var hello, bye string
+// определите переменные ver, id, pi
+	var ver = "v.0.0.1"
+	var id int
+	pi := 3.1415
 
-	// Присваиваем переменной number число 4
-	number = 4
+fmt.Println("ver =", ver, "id =", id, "pi =", pi)
 
-	// Присваиваем переменной hello строку "Привет!"
-	hello = "Привет!"
-	// Присваиваем переменной bye строку "Пока!"
-	bye = "Пока!"
-
-	// Выводим значения перменных на экран
-	fmt.Println("number = ", number)
-	fmt.Println("hello = ", hello)
-	fmt.Println("bye = ", bye)
 }
