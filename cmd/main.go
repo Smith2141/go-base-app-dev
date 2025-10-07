22ed0a1aff2ad9788afa5cd5355a3edaf4d96b3e
maxim <acer.laptop>
2025-10-07 20:57:50 +0300

    Задача 2/4

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	message := "Алиса, в какой папке находятся мои фото?"
+
+	// объявите переменную path и присвойте путь до папки в следующей строке
 	path := "C:\\Documents\\Photos"
 
+	fmt.Println(message)
+	// выведите путь до папки на экран ниже
 	fmt.Println(path)
+
 }
