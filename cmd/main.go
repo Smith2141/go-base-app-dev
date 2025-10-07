919e4adf4e007cb41231249d70cde31a4b91be58
maxim <acer.laptop>
2025-10-07 21:02:52 +0300

    Задача 2/4

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,23 @@
 package main
 
-import "fmt"
+// добавьте в импорт необходимый пакет
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	message := "Алиса, в какой папке находятся мои фото?"
 
-	// объявите переменную path и присвойте путь до папки в следующей строке
 	path := "C:\\Documents\\Photos"
 
-	fmt.Println(message)
-	// выведите путь до папки на экран ниже
-	fmt.Println(path)
+	// переведите все символы строки в переменной path в нижний регистр
+	// и присвойте результат переменной lowLetterPath
+	lowLetterPath := strings.ToLower(path)
 
+	fmt.Println(message)
+	fmt.Println("path =", path)
+
+	// выведите переменную lowLetterPath и ее значение на экран
+	fmt.Println("lowLetterPath =", lowLetterPath)
 }
