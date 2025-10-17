37ae0eb5a3260676fbbccd5fdae308f8a8cba5d8
Максим_work <berezovskiy_ma@mgtniip.ru>
2025-10-17 15:55:34 +0300

    main month-temperature

--- cmd/main.go
+++ cmd/main.go
@@ -2,21 +2,17 @@ package main
 
 import "fmt"
 
-const needVolume int32 = 5_000_000
-
 func main() {
-	var minSize int32 = 1_00
-	var maxSize int32 = 3_00
+	// массив для средней температуры по месяцам в городе Сочи
+	averageT := [12]float32{6.3, 6.5, 8.6, 12.3, 16.6, 20.9, 23.7,
+		24.3, 20.5, 16.2, 11.4, 8.3}
 
-main:
-	for i := minSize; i < maxSize; i++ {
-		var square int32 = i * i
-		for j := minSize; j < maxSize; j++ {
-			var volume int32 = square * j
-			if volume == needVolume {
-				fmt.Println(i, i, j, volume)
-				break main
-			}
-		}
+	months := [12]string{
+		"январь", "февраль", "март", "апрель", "май", "июнь",
+		"июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь",
+	}
+
+	for idx := range months {
+		fmt.Printf("%s %.1f\n", months[idx], averageT[idx])
 	}
 }
