30ea0d362c3199aa0b261da1bc2f281932595fcc
maxim <acer.laptop>
2025-10-20 12:11:51 +0300

    main fix letter 1

--- cmd/main.go
+++ cmd/main.go
@@ -2,37 +2,32 @@ package main
 
 import "fmt"
 
+const (
+	fixSymbol = '?'
+	cLetterEng = 'с'
+)
+
 func main() {
-	// оценки по отдельным ученикам
-	marks := [][]int{
-		{5, 4, 5, 5},
-		{3, 4, 4, 5, 3},
-		{2, 3, 3},
-		{5, 5, 4},
-		{4, 3, 4, 4, 3},
-	}
-	var average float32 // итоговый средний балл
+	words := []string{"сервер", "cистема", "специалист", "слайc", "процессор",
+		"масcив", "строка", "максимум", "cпоcоб", "парсер", "условие"}
+	var mistakes []string
 
-	// добавьте код для подсчёта среднего балла
-	for _, student := range marks {
-		average += calcAvg(student)
-	}
+	// вставьте недостающий код
+	for _, word := range words {
+		var hasMistake bool
+		runes := []rune(word)
+		for idx := range runes {
+			if runes[idx] == cLetterEng {
+				runes[idx] = fixSymbol
+				hasMistake = true
+				// fmt.Println("fixed: ", string(runes))
+			}
+		}
 
-	average /= float32(len(marks))
-
-	fmt.Printf("%.2f", average)
-}
-
-func calcAvg(markList []int) float32 {
-	var (
-		markSum int
-		result  float32
-	)
-
-	for _, mark := range markList {
-		markSum += mark
+		if hasMistake {
+			mistakes = append(mistakes, string(runes))
+		}
 	}
 
-	result = float32(markSum) / float32(len(markList))
-	return result
+	fmt.Println(mistakes)
 }
