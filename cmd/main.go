bfa91f2f7c47ecc67f4862710a934c90cf02e297
Maxim_10407 <berezovskiyma1@transport.mos.ru>
2025-10-21 12:54:36 +0300

    main maps soriting

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,40 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "sort"
+)
 
 func main() {
-	// оценки по отдельным ученикам
-	marks := map[string][]int{
-		"Светлана":  {5, 4, 5, 5, 4},
-		"Артём":     {3, 4, 4, 5, 3},
-		"Александр": {2, 3, 3, 4},
-		"Ольга":     {5, 5, 4, 4},
-		"Мария":     {4, 3, 4, 4, 3, 5},
-	}
-	student := "Светлана"
-	var average float32 // средний балл
-
-	// допишите недостающий код
-	studentMarks := marks[student]
-	studentMarks = append(studentMarks, 5)
-	var sum float32
-
-	for _,mark := range studentMarks {
-		sum += float32(mark)
-	}
-
-	average = sum / float32(len(studentMarks))
-
-	fmt.Printf("%.2f\n", average)
+    towns := map[string]int{
+        "Омск":      1104485,
+        "Киров":     475464,
+        "Астрахань": 465524,
+        "Пятигорск": 144955,
+        "Пенза2":     488299,
+        "Пенза1":     488299,
+        "Иркутск":   606369,
+    }
+    // создаём мапу с количеством жителей
+    // так как кол-во жителей у городов может совпадать,
+    // поэтому в качестве элементов мапы используется слайс, а не просто строка
+    counts := make(map[int][]string)
+    // слайс для количества жителей, который будет отсортирован
+    sorted := make([]int, 0, 10)
+    // формируем counts
+    for name, count := range towns {
+        _, ok := counts[count]
+        if !ok {
+            sorted = append(sorted, count)
+        }
+        counts[count] = append(counts[count], name)
+    }
+    // сортируем жителей
+    sort.Ints(sorted)
+    // выводим города по количеству жителей
+    for _, people := range sorted {
+        for _, town := range counts[people] {
+            fmt.Println(town, ":", people)
+        }
+    }
 }
