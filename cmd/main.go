11ebdcdc2a722a7ec3588fd6c150fa3c8f46024f
Maxim_10407 <berezovskiyma1@transport.mos.ru>
2025-10-22 12:37:05 +0300

    main add function printFriendsCount

--- cmd/main.go
+++ cmd/main.go
@@ -2,23 +2,29 @@ package main
 
 import "fmt"
 
+// объявите функцию здесь
+func printFriendsCount(friendsCount int) {
+	// код, написанный ниже, переместите внутрь объявленной вами функции
+	if friendsCount == 0 {
+		fmt.Println("У тебя нет друзей")
+	} else if friendsCount == 1 {
+		fmt.Println("У тебя", friendsCount, "друг")
+	} else if friendsCount >= 2 && friendsCount <= 4 {
+		fmt.Println("У тебя", friendsCount, "друга")
+	} else if friendsCount >= 5 && friendsCount < 20 {
+		fmt.Println("У тебя", friendsCount, "друзей")
+	} else {
+		fmt.Println("Ого, сколько у тебя друзей! Целых ", friendsCount)
+	}
+}
+
+// вызовите объявленную вами функцию printFriendsCount()
+// с нужными аргументами внутри функции main
 func main() {
-	titles := map[string][]string{
-		"Что делать?":               {"планы", "думы"},
-		"Где отдохнуть в выходные":  {"отдых", "планы"},
-		"Кто виноват?":              {"поиск", "думы", "общество"},
-		"Лучшие рестораны города":   {"еда", "отдых"},
-		"С заботой о народе":        {"думы", "общество"},
-		"Как попасть на дегустацию": {"еда", "планы", "отдых"},
-	}
-	tags := make(map[string][]string)
-
-	// вставьте недостающий код
-	for title := range titles {
-		for _, tag := range titles[title] {
-			tags[tag] = append(tags[tag], title)
-		}
-	}
-
-	fmt.Println(len(tags["думы"]), len(tags["общество"]), tags["поиск"])
+	// здесь вызовите функцию 5 раз, чтобы поочередно получить выводы из условия задания
+	printFriendsCount(0)
+	printFriendsCount(1)
+	printFriendsCount(2)
+	printFriendsCount(6)
+	printFriendsCount(20)
 }
