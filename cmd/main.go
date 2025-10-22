1c15dcb790018ada37726ff14340595b048be2a8
Maxim_10407 <berezovskiyma1@transport.mos.ru>
2025-10-22 17:57:39 +0300

    main functions rooms count

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,28 @@ package main
 
 import "fmt"
 
-// printGuests выводит заголовок и список гостей
-// определите параметры title и list
-func printGuests(title string, list ...string) {
-	// напишите тело функции
-	fmt.Println(title)
-	for num, guest := range list {
-		fmt.Println(num+1, guest)
-	}
+// объявите функцию roomsEqual() с параметрами roomSize и roomList
+// ...
+// перенесите следующий код в тело функции, которую вы объявили
+func roomsEqual(roomSize float64, roomList []float64)  {
+    count := 0
+    for _, room := range roomList {
+        if room == roomSize {
+            count = count + 1
+        }
+    }
+    fmt.Printf("Комнат площадью %.2f кв. м: %d\n", roomSize, count)
 }
 
 func main() {
-	printGuests("Список гостей на 4 августа", "Вера", "Ринат",
-		"Владимир", "Оксана", "Игорь")
+    flat := []float64{
+        5.55, 22.19, 7.78, 26.86, 5.55,
+        29.84, 22.19, 5.55, 16.85, 4.52,
+    }
+    hut := []float64{9.2, 3.5, 8.1, 2.3, 9.2, 4.2, 6.9}
+    
+    roomsEqual(5.55, flat)
+    // добавьте ещё один вызов функции roomsEqual()
+    // передайте в функцию искомую площадь 9.2 и слайс hut    
+    roomsEqual(9.2, hut)
 }
