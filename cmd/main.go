5563ac85f14ba77ac32d03504671882c029ad440
Maxim_10407 <berezovskiyma1@transport.mos.ru>
2025-10-22 17:45:11 +0300

    main functions list of friends

--- cmd/main.go
+++ cmd/main.go
@@ -2,26 +2,17 @@ package main
 
 import "fmt"
 
-// объявите функцию здесь
-func printFriendsCount(friendsCount int) {
-	// код, написанный ниже, переместите внутрь объявленной вами функции
-	if friendsCount == 0 {
-		fmt.Println("У тебя нет друзей")
-	} else if friendsCount == 1 {
-		fmt.Println("У тебя", friendsCount, "друг")
-	} else if friendsCount >= 2 && friendsCount <= 4 {
-		fmt.Println("У тебя", friendsCount, "друга")
-	} else if friendsCount >= 5 && friendsCount < 20 {
-		fmt.Println("У тебя", friendsCount, "друзей")
-	} else {
-		fmt.Println("Ого, сколько у тебя друзей! Целых ", friendsCount)
+// printGuests выводит заголовок и список гостей
+// определите параметры title и list
+func printGuests(title string, list ...string) {
+	// напишите тело функции
+	fmt.Println(title)
+	for num, guest := range list {
+		fmt.Println(num+1, guest)
 	}
 }
 
-// вызовите объявленную вами функцию printFriendsCount()
-// с нужными аргументами внутри функции main
 func main() {
-	for i := 0; i <= 20; i++ {
-		printFriendsCount(i)
-	}
+	printGuests("Список гостей на 4 августа", "Вера", "Ринат",
+		"Владимир", "Оксана", "Игорь")
 }
