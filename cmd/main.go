235f92d12927d73d6c0aacb9a53b34fa1eb721bf
maxim <acer.laptop>
2025-10-26 14:13:36 +0300

    main functions count letters in string

--- cmd/main.go
+++ cmd/main.go
@@ -2,28 +2,29 @@ package main
 
 import "fmt"
 
-// объявите функцию roomsEqual() с параметрами roomSize и roomList
-// ...
-// перенесите следующий код в тело функции, которую вы объявили
-func roomsEqual(roomSize float64, roomList []float64)  {
-    count := 0
-    for _, room := range roomList {
-        if room == roomSize {
-            count = count + 1
-        }
-    }
-    fmt.Printf("Комнат площадью %.2f кв. м: %d\n", roomSize, count)
+func numOfChar(letter rune, s string) {
+	// здесь объявите переменную count, равную нулю
+	// она будет хранить количество вхождений
+	var count int
+	for _, ch := range s {
+		// напишите условие: сравните переменные letter и ch
+		// и если letter равна ch, увеличивайте счётчик count на 1
+		if letter == rune(ch) {
+			count++
+		}
+	}
+	// печатаем исходную строку:
+	fmt.Println("Исходная строка:", s)
+	// печатаем результат подсчётов:
+	fmt.Printf("Количество вхождений символа %c составляет %d\n", letter, count)
 }
 
 func main() {
-    flat := []float64{
-        5.55, 22.19, 7.78, 26.86, 5.55,
-        29.84, 22.19, 5.55, 16.85, 4.52,
-    }
-    hut := []float64{9.2, 3.5, 8.1, 2.3, 9.2, 4.2, 6.9}
-    
-    roomsEqual(5.55, flat)
-    // добавьте ещё один вызов функции roomsEqual()
-    // передайте в функцию искомую площадь 9.2 и слайс hut    
-    roomsEqual(9.2, hut)
+	// код ниже не изменяйте
+	phrase := "Не волнуйтесь, если что-то не работает. " +
+		"Если бы всё работало, вас бы уволили."
+
+	// вызываем функцию numOfChar(), чтобы она посчитала,
+	// сколько раз во фразе phrase встречается буква 'е'
+	numOfChar('е', phrase)
 }
