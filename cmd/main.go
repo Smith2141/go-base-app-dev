a56b80a0a9ca81e53d329a91b313c856858591c1
Maxim_10407 <berezovskiyma1@transport.mos.ru>
2025-10-30 09:52:06 +0300

    main function get function as argument

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-// функция для вычисления периметра куба
-func calcCubePerimeter(side float64) float64 {
-	return side * 12
+// Функция, которая принимает другую функцию в качестве аргумента
+func squareAndPrint(fn func(int) int) {
+	result := fn(5)
+	fmt.Println(result)
 }
 
-// функция для вычисления площади куба
-func calcCubeArea(side float64) float64 {
-	oneFace := side * side
-	return oneFace * 6
-}
-
-func calcCube(side float64, amount int) {
-	oneCubePerimeter := calcCubePerimeter(side)
-	fullLength := oneCubePerimeter * float64(amount)
-	oneCubeArea := calcCubeArea(side)
-	fullArea := oneCubeArea * float64(amount)
-
-	fmt.Printf("Для %d кубов с ребром %.2f м понадобится"+
-		" %.2f м палок и %.2f кв. м стекла\r\n",
-		amount, side, fullLength, fullArea)
+// Функция, которая будет передаваться как аргумент
+func square(x int) int {
+	return x * x
 }
 
 func main() {
-	// ниже напишите три вызова функции calcСube(),
-	// каждый вызов должен быть на отдельной строке
-
-	calcCube(2, 4)
-	calcCube(0.5, 26)
-	calcCube(0.61, 6)
+	// Передаем функцию square в doSomething
+	squareAndPrint(square)
 }
