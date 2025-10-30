a818e05ccc5fbb33f2573c0d4b7310db22323aa5
Maxim_10407 <berezovskiyma1@transport.mos.ru>
2025-10-30 09:58:23 +0300

    main anonymous function to function

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,17 @@ package main
 
 import "fmt"
 
+// printOnTrue напечатает s в случае, если функция f вернёт true,
+// в противном случае она ничего не сделает
+func printOnTrue(f func(a int, b int) bool, s string) {
+    if f(10, 20) {
+        fmt.Println(s)
+    }
+}
+
 func main() {
     // создаём анонимную функцию и присваиваем переменной f
     f := func(a, b int) bool { return a < b }
-    // вызываем функцию и сохраняем результат в переменную res
-    res := f(10, 20)                         
-
-    fmt.Println(res)
+    // передаём функцию f в качестве аргумента функции printOnTrue
+    printOnTrue(f, "и правда меньше")
 }
